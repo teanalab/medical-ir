6b582960d823ee6c199bd845b0021044e17bfaea
fsqcds <fsqcds@gmail.com>
2014-08-01 18:27:34 -0400

    extract relationships from sql db

--- medical-ir-clj/src/medical_ir_clj/relationship_util.clj
+++ medical-ir-clj/src/medical_ir_clj/relationship_util.clj
@@ -1,29 +1,9 @@
 (ns medical-ir-clj.relationship-util
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [medical-ir-clj.umls-db.entities :refer :all]
+            [korma.core :refer :all]))
 
-(defn ro-relationship-here?
-  [[_ _ _ rel _ _ _ rela]]
-  (and
-   (= rel "RO")
-   ((complement str/blank?) rela)))
-
-(defn concepts-and-relationship
-  [[cui1 _ _ _ cui2 _ _ rela]]
-  [cui1 cui2 rela])
-
-(def relationships
-  (with-open [rdr (io/reader "../META/MRREL.RRF")]
-    (doall (map concepts-and-relationship
-                (filter ro-relationship-here?
-                        (map #(str/split % #"\|")
-                             (take 500 (line-seq rdr))))))))
-
-(def concepts->relationship
-  (into {} (map (fn [[cui1 cui2 rela]] [[cui1 cui2] rela]) relationships)))
-
-(def concept-relationship->concepts
-  (apply merge-with
-         (partial merge-with set/union)
-         (map (fn [[cui1 cui2 rela]] {cui1 {rela #{cui2}}}) relationships)))
+(defn get-relations
+  [concept-id]
+  (select mrrel
+          (fields :cui2 :rel :rela)
+          (where {:cui1 concept-id})))

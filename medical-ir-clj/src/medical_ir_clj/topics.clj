5ae39da148ead6ecd23700ec0cf08b12dab89bc3
fsqcds <fsqcds@gmail.com>
2014-08-04 13:54:13 -0400

    (print-topics)

--- medical-ir-clj/src/medical_ir_clj/topics.clj
+++ medical-ir-clj/src/medical_ir_clj/topics.clj
@@ -3,26 +3,62 @@
             [clojure.xml :as xml]
             [clojure.zip :as zip]
             [clojure.data.zip.xml :as zip-xml]
+            [clojure.pprint :refer [print-table]]
             [medical-ir-clj.concept-extraction :refer :all]
+            [medical-ir-clj.concept-util :refer :all]
             [medical-ir-clj.relationship-util :refer :all]))
 
 (def topics-xml (-> "topics.xml" io/resource io/file xml/parse zip/xml-zip))
 
 (def topics
   (for [m (zip-xml/xml-> topics-xml :topic)]
-    {:type (zip-xml/attr m :type) :text (zip-xml/xml1-> m :summary zip-xml/text)}))
+    {:type (zip-xml/attr m :type)
+     :text (zip-xml/xml1-> m :summary zip-xml/text)}))
 
 (defn extract-concepts
   [topics]
   (map #(assoc % :concepts (get-concepts-with-names (:text %))) topics))
 
-(defn concepts-add-relations
+(defn concepts-add-relationships
   [concepts]
   (for [concept concepts]
-    (assoc concept :relations (map #(assoc % :concept-name (concept-name (:cui2 %)))(get-relations (:concept-id concept))))))
+    (assoc concept :relationships
+           (map #(assoc % :concept-name (concept-name (:cui2 %)))
+                                   (get-relations (:concept-id concept))))))
 
 (defn extract-relations
   [topics]
   (for [topic (extract-concepts topics)
         :let [concepts {:concepts topic}]]
-    (assoc topic :concepts (concepts-add-relations (:concepts topic)))))
+    (assoc topic :concepts (concepts-add-relationships (:concepts topic)))))
+
+;;; Printing
+
+(def indent-str "  ")
+(defn print-indent
+  [indent-level]
+  (print (apply str (repeat indent-level indent-str))))
+
+(defn print-relationship
+  [{:keys [concept-name rel rela]} indent-level]
+  (print-indent indent-level)
+  (println (str concept-name "\tREL: " rel "\tRELA: " rela)))
+
+(defn print-concept
+  [{:keys [concept-name relationships]} indent-level]
+  (print-indent indent-level)
+  (println concept-name)
+  (doseq [relationship relationships]
+    (print-relationship relationship (inc indent-level))))
+
+(defn print-topic
+  [{:keys [type text concepts]} indent-level]
+  (println (str "Type: " type))
+  (println (str "Text: " text))
+  (doseq [concept concepts]
+    (print-concept concept (inc indent-level))))
+
+(defn print-topics
+  []
+  (doseq [topic (extract-relations topics)]
+    (print-topic topic 0)))

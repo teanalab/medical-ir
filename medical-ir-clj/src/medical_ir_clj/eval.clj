ac965b7b02553402daa4ffcc0602dd360499512f
fsqcds <fsqcds@gmail.com>
2014-08-18 19:02:17 -0400

    eval.clj main functions to create queries and qrel files

--- medical-ir-clj/src/medical_ir_clj/eval.clj
+++ medical-ir-clj/src/medical_ir_clj/eval.clj
@@ -7,6 +7,8 @@
   (:import java.io.PrintStream
            org.lemurproject.galago.core.tools.App))
 
+(def default-expansion-function expand-text-sequential-mrf)
+
 (def eval-topics (filter #(<= (:number %) 10) topics))
 
 (defn eval-topics-expanded-json
@@ -15,32 +17,54 @@
                             :text (query-expansion-function (:text %)))
                  eval-topics)})
 
-(defn queries-json-file
+(defn queries-json
+  "constructs json string for queries"
+  [query-expansion-function]
+  (json/write-str (eval-topics-expanded-json query-expansion-function)))
+
+(defn queries-json-tmp-file
+  "create temprorary file with queries and return it"
   [query-expansion-function]
   (let [queries-json-file (java.io.File/createTempFile "queries" ".json")]
-    (spit queries-json-file
-          (json/write-str (eval-topics-expanded-json query-expansion-function)))
+    (spit queries-json-file (queries-json query-expansion-function))
     queries-json-file))
 
+(defn queries-json-file
+  "make queries file with required filename"
+  [filename]
+  (spit filename (queries-json default-expansion-function)))
+
 (defn batch-search
-  [query-expansion-function]
-  (let [queries-json-file (queries-json-file query-expansion-function)
-        search-results (java.io.File/createTempFile "search-results" ".json")]
+  [query-expansion-function print-stream]
+  (let [queries-json-file (queries-json-tmp-file query-expansion-function)]
     (App/run (into-array ["batch-search"
                           (str "--index=" (:galago-index-path config))
                           (str queries-json-file)])
-      (PrintStream. search-results))
-    (.delete queries-json-file)
+      print-stream)
+    (.delete queries-json-file)))
+
+(defn batch-search-tmp-file
+  [query-expansion-function]
+  (let [search-results (java.io.File/createTempFile "search-results" ".json")]
+    (batch-search query-expansion-function (PrintStream. search-results))
     search-results))
+
+(defn batch-search-file
+  [filename]
+  (batch-search default-expansion-function (PrintStream. filename)))
 
 (defn evaluate
   [query-expansion-function judgements-filepath]
-  (let [batch-search-results (batch-search query-expansion-function)]
+  (let [batch-search-results (batch-search-tmp-file query-expansion-function)]
     (App/main (into-array ["eval"
                            (str "--judgments=" judgements-filepath)
                            (str "--baseline=" batch-search-results)]))))
 
+
 (defn -main
   [& args]
-  (evaluate expand-text-sequential-mrf (-> "qrelsDS.txt" io/resource io/file str))
-  (evaluate expand-text-sequential-mrf (-> "qrelsLDA.txt" io/resource io/file str)))
+  (println "qrelsLDA.txt: ")
+  (evaluate default-expansion-function (-> "qrelsLDA.txt" io/resource io/file str))
+  (println)
+  (println "qrelsDS.txt: ")
+  (evaluate default-expansion-function (-> "qrelsDS.txt" io/resource io/file str)))

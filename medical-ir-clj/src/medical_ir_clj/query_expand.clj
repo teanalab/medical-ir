bc0eb32ec69427a05cecd32aa0c2416adcd38bf9
fsqcds <fsqcds@gmail.com>
2014-08-18 20:46:50 -0400

    Use galago traversal instead of modifying query

--- medical-ir-clj/src/medical_ir_clj/query_expand.clj
+++ medical-ir-clj/src/medical_ir_clj/query_expand.clj
@@ -13,22 +13,17 @@
   [tokens]
   (str "#combine(" (str/join " " tokens) ")"))
 
-(defn expand-tokens-bigrams
-  [tokens operator]
-  (str "#combine("
-       (str/join " "
-                 (map #(str operator "(" (str/join " " %) ")")
-                      (partition 2 1 tokens))) ")"))
-
 (defn expand-tokens-sequential-mrf
   [tokens]
-  (str "#combine:0=0.8:1=0.1:2=0.1( " (expand-tokens-combine tokens) " "
-       (expand-tokens-bigrams tokens "#od:1") " "
-       (expand-tokens-bigrams tokens "#uw:8") " )"))
+  (str "#sdm(" (str/join " " tokens) ")"))
 
 (defn expand-text-query
   [query expand-tokens-function]
   (-> query remove-punctuation tokenize expand-tokens-function))
+
+(defn expand-text-combine
+  [query]
+  (expand-text-query query expand-tokens-combine))
 
 (defn expand-text-sequential-mrf
   [query]
